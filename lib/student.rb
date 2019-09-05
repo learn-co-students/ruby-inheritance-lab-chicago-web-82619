d089516f1303bd59496420f04cb31f5f4a5b2a78
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-09-05 09:22:23 -0500

    lab complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(wisdom)
+    self.knowledge.push(wisdom)
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
